f6462b0b89da8e8b24c3794691fbaa8551d5e6f3
uusuri <kunitski@mail.ru>
2026-06-30 11:15:10 +0300

    Залил ауф так называемый

---
+++ backend/src/main/java/com/qlc/services/UserService.java
@@ -0,0 +1,29 @@
+package com.qlc.services;
+
+import com.qlc.models.entities.User;
+import com.qlc.repositories.UserRepository;
+import com.qlc.security.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+  public UserRepository userRepository;
+
+  @Autowired
+  public void setUserRepository(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+    return UserDetailsImpl.build(user);
+  }
+}
